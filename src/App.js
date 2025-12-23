2b933ebb80cb20ba6fcda834173e6d901a6f92ba
francojjuarez61-dev <francojjuarez61@gmail.com>
2025-12-23 13:02:15 -0300

    Create package.json for project setup
    Initialized package.json with app metadata and dependencies.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+{
+  "name": "imperio-infernal-app",
+  "version": "1.0.0",
+  "private": true,
+  "dependencies": {
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "lucide-react": "^0.263.1",
+    "firebase": "^10.0.0"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build"
+  }
+}
